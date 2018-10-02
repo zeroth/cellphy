75d6b0792b99ad497425455224fb34def911db2b
Abhishek Patil <abhishek.patil@student.unsw.edu.au>
2018-10-02 11:55:15 +1000

    moved few more things to linux only
    Signed-off-by: Abhishek Patil <abhishek.patil@student.unsw.edu.au>

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import argparse
 import os
 import sys
-from cellphy.lib import libinstall
-from cellphy.Bleaching import bleach_dir
 from cellphy.Analysis.Tools import calculate_msd, calculate_msb_by_bin, track_analyze, analyze_mean_stddev
 from cellphy.tracking_analyzer_gui import start_ui
 
@@ -13,6 +11,8 @@ if sys.platform.startswith('linux'):
 if is_linux:
     from cellphy.preprocessing import deskew_dir
     from cellphy.preprocessing import decon_dir
+    from cellphy.lib import libinstall
+    from cellphy.Bleaching import bleach_dir
 
 
 def process_args():
@@ -21,16 +21,11 @@ def process_args():
     commands = arguments.add_subparsers(dest='func')
 
     a_arguments = commands.add_parser('analysis')
-    b_arguments = commands.add_parser('bleach')
 
     m_arguments = commands.add_parser('msd')
     bm_arguments = commands.add_parser('binmsd')
     t_arguments = commands.add_parser('track')
     ui_arguments = commands.add_parser('gui')
-
-    # bleach
-    b_arguments.add_argument('path', default='.')
-    b_arguments.set_defaults(func=bleach_dir)
 
     # Analysis
     a_arguments.add_argument('path', default='.')
@@ -55,25 +50,30 @@ def process_args():
 
     if is_linux:
         # specific to linux
-        d_arguments = commands.add_parser('deskew')
-        i_arguments = commands.add_parser('install')
-        dc_arguments = commands.add_parser('decon')
 
         # install
+        i_arguments = commands.add_parser('install')
         i_arguments.add_argument('path', default='.')
         i_arguments.set_defaults(func=install)
 
         # decon
+        dc_arguments = commands.add_parser('decon')
         dc_arguments.add_argument('path', default='.')
         dc_arguments.add_argument('--channel', type=int, required=True)
         dc_arguments.set_defaults(func=decon_dir)
 
         # deskew
+        d_arguments = commands.add_parser('deskew')
         d_arguments.add_argument('path', default='.')
         d_arguments.add_argument('--angle', default=31.5, type=float)
         d_arguments.add_argument('--dz', default=0.4, type=float)
         d_arguments.add_argument('--pixelsize', default=0.104, type=float)
         d_arguments.set_defaults(func=deskew_dir)
+
+        # bleach
+        b_arguments = commands.add_parser('bleach')
+        b_arguments.add_argument('path', default='.')
+        b_arguments.set_defaults(func=bleach_dir)
 
 
     args = arguments.parse_args()
